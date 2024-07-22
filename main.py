df24296f2df57f6cc7817bea3bf22ebb179fbbc6
Dmytro <2937451+vorotech@users.noreply.github.com>
2024-07-22 12:10:10 +0300

    add task 1

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import networkx as nx
+import random
+
 import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 from scipy.interpolate import interp1d
 
-# Функція для апроксимації та розрахунку довжини кривої
+
+# Функція для апроксимації кривої
 def interpolate_curve(x_points, y_points, num_points=100):
     t = np.linspace(0, 1, len(x_points))
     interp_x = interp1d(t, x_points, kind='linear')
@@ -56,12 +59,12 @@ stations = [
 G.add_nodes_from(stations)
 
 # Визначення контрольних точок для кривих (приблизні координати)
-red_line_x = [1,  6, 15, 17, 19, 21]
+red_line_x = [-2,  6, 15, 17, 20, 25]
 red_line_y = [16, 11, 11,  9,  9, 12]
-blue_line_x = [13, 13, 11, 9, 5, 0]
-blue_line_y = [25, 10, 8, 5, 1, 1]
-green_line_x = [6, 11, 11, 13, 13, 17, 21, 25]
-green_line_y = [17, 14, 10, 8,  5,  1,  1,  3]
+blue_line_x = [13, 13, 11, 9, 4, -4]
+blue_line_y = [30, 10, 8, 5, 1, 1]
+green_line_x = [4, 10, 10, 13, 13, 17, 24, 30]
+green_line_y = [18, 14, 10, 8,  5,  1,  1,  3]
 
 # Генерація позицій для кожної лінії
 red_line_positions = generate_uniform_curve_positions(["110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122", "123", "124", "125", "126", "127"], red_line_x, red_line_y)
@@ -85,13 +88,49 @@ edges = [
     ("119", "314"), ("315", "218"), ("120", "217")
 ]
 
-G.add_edges_from(edges)
+random.seed(42)
+
+# Додавання ребер до графа з атрибутами
+for edge in edges:
+    random_weight = round(random.uniform(0.5, 2), 1)
+    if edge[0] in red_line_positions and edge[1] in red_line_positions:
+        G.add_edge(edge[0], edge[1], color='red', weight=random_weight)
+    if edge[0] in blue_line_positions and edge[1] in blue_line_positions:
+        G.add_edge(edge[0], edge[1], color='blue', weight=random_weight)
+    if edge[0] in green_line_positions and edge[1] in green_line_positions:
+        G.add_edge(edge[0], edge[1], color='green', weight=random_weight)
+
+# Додавання ребер переходів між лініями до графа з атрибутами
+connections = [("119", "314", 'purple', 5), ("315", "218", 'purple', 7), ("120", "217", 'purple', 4)]
+for c in connections:
+    G.add_edge(c[0], c[1], color=c[2], weight=c[3])
+
+
+# Отримання кольорів та ваг ребер
+edge_colors = [G[u][v]['color'] for u, v in G.edges()]
+edge_weights = [G[u][v]['weight'] for u, v in G.edges()]
 
 # Візуалізація графа
 plt.figure(figsize=(10, 7))
-nx.draw(G, pos=positions, with_labels=True, node_color='skyblue', node_size=500, font_size=10, font_color='black', edge_color='gray')
+nx.draw(G, pos=positions, with_labels=True, node_color='skyblue', node_size=500, font_size=10, font_color='black', edge_color=edge_colors, width=edge_weights)
 
-# Відображення кривих ліній для наочності (debug)
+
+# Аналіз основних характеристик
+num_nodes = G.number_of_nodes()
+num_edges = G.number_of_edges()
+degrees = dict(G.degree())
+degree_centrality = nx.degree_centrality(G)
+closeness_centrality = nx.closeness_centrality(G)
+betweenness_centrality = nx.betweenness_centrality(G)
+
+print(f"К-ть вершин: {num_nodes}")
+print(f"К-ть ребер: {num_edges}")
+print(f"max та min ступені вершин: {max(degrees.values())}, {min(degrees.values())}")
+print(f"Ступінь центральності top10: {sorted(degree_centrality.items(), key=lambda x: x[1], reverse=True)[:10]}")
+print(f"Близькість вузла top10: {sorted(closeness_centrality.items(), key=lambda x: x[1], reverse=True)[:10]}")
+print(f"Посередництво вузла top10: {sorted(betweenness_centrality.items(), key=lambda x: x[1], reverse=True)[:10]}")
+
+# Відображення кривих ліній для debug
 #plt.plot(red_line_x, red_line_y, 'r--', alpha=0.5)
 #plt.plot(blue_line_x, blue_line_y, 'b--', alpha=0.5)
 #plt.plot(green_line_x, green_line_y, 'g--', alpha=0.5)
